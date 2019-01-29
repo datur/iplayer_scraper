7ed6d6ee9555907106837e7eb29639f308e659f1
David Turner <davidturner@MacBook-Pro-8.local>
2019-01-29 14:08:17 +0000

    updated init

--- browser_class.py
+++ browser_class.py
@@ -1,17 +1,18 @@
 import mechanicalsoup
 
+
 class Browser(object):
-	"""docstring for Browser"""
-	def __init__(self):
-		self.browser = mechanicalsoup.StatefulBrowser()
-		self.current_url = None
+    """docstring for Browser"""
 
+    def __init__(self):
+        self.browser = mechanicalsoup.StatefulBrowser()
+        self.current_url = None
+        self._BASE_URL = 'https://bbc.co.uk'
 
-	def get_page(self ,url):
-		self.browser.open(url)
-		self.current_url = url
-		return self.browser.get_current_page()
+    def get_page(self, url):
+        self.browser.open(url)
+        self.current_url = url
+        return self.browser.get_current_page()
 
-	def get_url(self):
-		return self.url
-
+    def get_url(self):
+        return self.current_url
