6e0f3e5bdb67249d27c0ef14e950c93d591091e5
David Turner <davidpaulturner350@gmail.com>
2019-01-31 10:46:41 +0000

    updates

--- extractor_class.py
+++ extractor_class.py
@@ -67,7 +67,7 @@ class Extractor(object):
 
         recommendations = self.iplayer_recmmendations(web_page)
 
-        available_episodes = self.available_episodes(web_page)
+        available_episodes = self.episode_available_extraction§(web_page)
 
         # return this as a dictionary and use a dictionary update
 
