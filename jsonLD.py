b83e245894c15f1dc3964a7acbdc26754373fd07
David Turner <davidpaulturner350@gmail.com>
2019-02-19 22:37:40 +0000

    initial commit

---
+++ jsonLD.py
@@ -0,0 +1,11 @@
+import  json
+from pyld import jsonld
+
+context = {
+        "@contest": "https://www.bbc.co.uk/ontologies/po"
+            "@title": "Brand"
+            "synopsis"
+            "recommendtions"
+            "program_website_url"
+            "program_credits_url"
+            }
